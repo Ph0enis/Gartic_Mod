ac0c8daa4796ee689fb38752a20f62ed8c916973
aaron <aaronwesterman66@yahoo.com>
2024-08-12 13:48:11 +0900

    My First working item in minecraft

---
+++ src/main/java/github/Ph0enis/Gartic/Gartic.java
@@ -0,0 +1,17 @@
+package github.Ph0enis.Gartic;
+
+import github.Ph0enis.Gartic.init.ModItems;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Gartic.MODID)
+public class Gartic {
+    public static final String MODID = "gartic";
+
+    public Gartic() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.ITEMS.register(bus);
+    }
+}
---
+++ src/main/java/github/Ph0enis/Gartic/init/ModItems.java
@@ -0,0 +1,25 @@
+package github.Ph0enis.Gartic.init;
+
+import github.Ph0enis.Gartic.Gartic;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Gartic.MODID);
+
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(16)
+                    .food(new FoodProperties.Builder()
+                            .nutrition(5)
+                            .saturationModifier(.2f)
+                            .build())
+                    .rarity(Rarity.EPIC)
+            ));
+}
